76cd572e6dff943e8e112f1cf5a604e7f7a30e4c
mdNazmulHasan <najmul.hasan10@gmail.com>
2017-01-14 10:32:38 +0600

    now image is saved in picture folder

--- app/src/main/java/com/example/user/cameraexample/MainActivity.java
+++ app/src/main/java/com/example/user/cameraexample/MainActivity.java
@@ -12,64 +12,103 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView imageView;
-    private TextView photoPathTV;
-    private File photoDirectory,mediaFile;
-    private Uri photoUri;
-    private String imageDirectoryName,imageFileName;
-    private static final int IMAGE_REQUEST = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView mIvPhotoView = null;
+    String mCurrentPhotoPath;
+    Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = (ImageView) findViewById(R.id.myImage);
-        photoPathTV = (TextView) findViewById(R.id.photoPathTV);
-        photoDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        //photoDirectory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),imageDirectoryName);
-        if(!photoDirectory.exists()){
-            if(!photoDirectory.mkdir()){
-                //Log.e(imageDirectoryName, "failed to create "+imageDirectoryName);
-                Log.e("Failure", "Failed to create directory ");
+        mIvPhotoView = (ImageView) findViewById(R.id.myImage);
+    }
+
+    public void dispatchTakePictureIntent(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try
+            {
+                photoFile = createImageFile();
             }
+            catch (IOException ex)
+            {
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT)
+                        .show();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null)
+            {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment
+                .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+
+        /**************************
+         |---- You will get the image location from this variable which you will save into database
+         */
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
+        {
+            setPic();
         }
     }
 
-    public void takePhoto(View view) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(intent.resolveActivity(getPackageManager()) != null){
-            File photoFile = null;
-            photoFile = createImageFile();
-            if(photoFile != null){
-                photoUri = Uri.fromFile(photoFile);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
-                startActivityForResult(intent,IMAGE_REQUEST);
-            }
-        }
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == IMAGE_REQUEST){
-            if(resultCode == RESULT_OK){
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inSampleSize = 8;
-                Bitmap bitmap = BitmapFactory.decodeFile(photoUri.getPath(),options);
-                imageView.setImageBitmap(bitmap);
-                photoPathTV.setText(photoUri.getPath());
-            }
-        }
-    }
-    private File createImageFile(){
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        imageFileName = "JPEG_"+timeStamp+".jpg";
-        mediaFile = new File(photoDirectory.getPath(),imageFileName);
-        return mediaFile;
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = mIvPhotoView.getWidth();
+        int targetH = mIvPhotoView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        mBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);// bmOptions
+        mIvPhotoView.setImageBitmap(mBitmap);
     }
 }
